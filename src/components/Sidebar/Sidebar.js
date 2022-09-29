c0fcb74e130a85719b2a205f80fe4cc934b0de09
noor16mustafa <c173212@ugrad.iiuc.ac.bd>
2022-09-29 13:27:20 +0600

    sidebar added

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,1 +1,56 @@
+import './Sidebar.css'
 
+const Sidebar = (props) => {
+
+
+
+
+
+    //console.log(total);
+
+
+    return (
+        <div className='sidebar-container'>
+            <div className="my-info">
+                <img src="https://cdn3.iconfinder.com/data/icons/business-avatar-1/512/4_avatar-128.png" alt="" />
+                <div className="my-details">
+                    <h3> Dilshad Noor Mustafa</h3>
+                    <h4> Completed B.SC in CSE</h4>
+                </div>
+
+            </div>
+            <div>
+                <h2>Add A Break</h2>
+                <div className="break-btn">
+                    <button className='btn'><span>10</span>m</button>
+                    <button className='btn'><span>20</span>m</button>
+                    <button className='btn'><span>30</span>m</button>
+                    <button className='btn'><span>40</span>m</button>
+                </div>
+            </div>
+            <div>
+                <h2>Exercise Details</h2>
+                <div className="exercise">
+                    <div className="exercise-text">
+                        <p>Exercise time</p>
+                    </div>
+                    <div className="exercise-time"><p>  minute</p></div>
+                </div>
+            </div>
+            <div>
+
+                <div className="break-container">
+                    <div className="break-text">
+                        <p>Break Time</p>
+                    </div>
+                    <div className="break-time"><p>minute</p></div>
+                </div>
+            </div>
+            <div className='activity-completed'>
+                <button className='completed-btn'>Activity Completed</button>
+            </div>
+        </div>
+    );
+};
+
+export default Sidebar;
