320fd8d9ab3b9bfa5211dc4324dd02d836e8cef9
noor16mustafa <c173212@ugrad.iiuc.ac.bd>
2022-09-29 13:20:03 +0600

    card completed

--- src/components/Activities/Activities.js
+++ src/components/Activities/Activities.js
@@ -1,16 +1,19 @@
 import React from 'react';
+import './Activities.css'
 
 const Activities = (props) => {
-    const { img, description, name, time } = props.activity;
+    const { handleAddBtn, activity } = props;
+    const { img, description, name, time } = activity;
 
     return (
-        <div>
+        <div className="activity-card">
             <img src={img} alt="" />
-            <div>
+            <div className="activity-info">
                 <h2>{name}</h2>
                 <p>{description}</p>
-                <h4>Time requires: {time}</h4>
+                <h4>Time requires: {time}min</h4>
             </div>
+            <button onClick={() => handleAddBtn(activity)} className='btn-add'><p className='btn-text'>Add To List</p></button>
         </div>
     );
 };
--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,21 +1,39 @@
 import React, { useEffect, useState } from 'react';
 import Activities from '../Activities/Activities';
+import Sidebar from '../Sidebar/Sidebar';
+import './Card.css'
 
 const Card = () => {
     const [activities, setActivities] = useState([]);
+    const [time, setTime] = useState([]);
 
     useEffect(() => {
         fetch('data.json')
             .then(res => res.json())
             .then(data => setActivities(data))
     }, [])
-    return (
-        <div>
-            {
-                activities.map(activity =>
-                    <Activities activity={activity}></Activities>)
 
-            }
+    const handleAddBtn = (selectedActivity) => {
+        const newArr = [...time, selectedActivity]
+        //console.log(newArr);
+        setTime(newArr);
+    }
+    return (
+        <div className='activities'>
+            <div className="activity-container">
+                {
+                    activities.map(activity =>
+                        <Activities
+                            activity={activity}
+                            key={activity.id}
+                            handleAddBtn={handleAddBtn}></Activities>)
+
+                }
+            </div>
+            <div className="sidebar">
+                <Sidebar selectedActivity={time}></Sidebar>
+            </div>
+
         </div>
     );
 };
